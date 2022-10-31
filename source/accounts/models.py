0e4681580cdcfa7e0f99a26eebefe753dfdc8ced
Isa Bakhtov <issab79@icloud.com>
2022-10-31 20:43:50 +0600

    1.Внесены дополнительная поля в модели accounts: имя, номер телефона, пол 2.Соответсвенно для отобрадения их в шаблоне внесены изменения в формы 3.Проеведена миграция

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,7 +1,12 @@
+from django.db.models import TextChoices
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from accounts.managers import UserManager
+
+class GenderChoices(TextChoices):
+    MAEL = 'male', 'мужчина'
+    FEMALE = 'female', 'женшина'
 
 
 class Account(AbstractUser):
@@ -18,10 +23,46 @@ class Account(AbstractUser):
         blank=True,
         verbose_name='Дата рождения'
     )
-    # liked_posts = models.ManyToManyField(verbose_name='Понравившиеся публикации', to='posts.Post', related_name='user_likes')
-    # subscriptions = models.ManyToManyField(verbose_name='Подписки', to='accounts.Account', related_name='subscribers')
-    # commented_posts = models.ManyToManyField('Прокомментированные публикации', to='posts.Post', related_name='user_comments')
-
+    
+    liked_posts = models.ManyToManyField(
+        verbose_name='Понравившиеся публикации',
+        to='posts.Post',
+        related_name='user_likes'
+    )
+    subscriptions = models.ManyToManyField(
+        verbose_name='Подписки',
+        to='accounts.Account',
+        related_name='subscribers'
+    )
+    commented_posts = models.ManyToManyField(
+        verbose_name='Прокомментированные публикации',
+        to='posts.Post',
+        related_name='user_comments'
+    )
+    user_info = models.CharField(
+        verbose_name='Информация о пользователе',
+        null=True,
+        blank=True,
+        max_length=200
+    )
+    phone_num = models.IntegerField(
+        verbose_name = 'Номер телефона',
+        null=True,
+        blank=True,
+        default=None
+    )
+    gender = models.CharField(
+        verbose_name='Пол',
+        choices=GenderChoices.choices,
+        max_length=100,
+        default='Gender did not choose')
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    changed_at = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
+    deleted_at = models.DateTimeField(verbose_name='Дата удаления', null=True, default=None)
+    is_deleted = models.BooleanField(verbose_name="Удалено", default=False, null=False)
+    
+    
+    
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
--- source/posts/models.py
+++ source/posts/models.py
@@ -1,3 +1,31 @@
+from venv import create
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    description = models.CharField(verbose_name='Описание', null=False, blank=False, max_length=200)
+    image = models.ImageField(verbose_name='Фото', null=False, blank=False, upload_to='posts')
+    author = models.ForeignKey(verbose_name='Автор', to=get_user_model(), related_name='posts', null=False, blank=False,
+                               on_delete=models.CASCADE)
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    changed_at = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
+    deleted_at = models.DateTimeField(verbose_name='Дата удаления', null=True, default=None)
+    is_deleted = models.BooleanField(verbose_name="Удалено", default=False, null=False)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(verbose_name='Автор', to=get_user_model(), related_name='comments', null=False,
+                               blank=False,
+                               on_delete=models.CASCADE)
+    post = models.ForeignKey(verbose_name='Публикация', to='posts.Post', related_name='comments', null=False,
+                             blank=False, on_delete=models.CASCADE)
+    text = models.CharField(verbose_name='Текст', null=False, blank=False, max_length=200)
+
+
+# class Like(models.Model):
+#     liked_by = models.ForeignKey(verbose_name='Автор', to=get_user_model(), related_name='likes', null=False,
+#                                blank=False,
+#                                on_delete=models.CASCADE)
+#     post = models.ForeignKey(verbose_name='Публикация', to='posts.Post', related_name='likes', null=False,
+#                              blank=False, on_delete=models.CASCADE)
